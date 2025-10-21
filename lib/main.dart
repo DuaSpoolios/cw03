37a899aecc06745f48c2313a2a459f48deadd061
DuaSpoolios <duaspall372332@gmail.com>
2025-10-20 20:40:30 -0400

    first part

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TaskApp());
+}
+
+class TaskApp extends StatelessWidget {
+  const TaskApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const TaskListScreen(),
+    );
+  }
+}
+
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key});
+
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Task Manager')),
+      body: const Center(
+        child: Text('No tasks yet.'),
+      ),
+    );
+  }
+}
